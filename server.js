e4011ce5bb6e5c2195e26695f412e6c9d923e5ba
pku <maciejbal90@gmail.com>
2016-08-16 14:39:16 +0200

    First mongodb connection.

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 "use strict";
 var express = require('express');
 var path = require('path');
+var mongodb = require('mongodb');
+var assert = require('assert');
+var MongoClient = mongodb.MongoClient;
+var dbUrl = 'mongodb://localhost:27017/qcms';
 var app = express();
+app.listen(3000);
 app.use(express.static(path.resolve(__dirname)));
 app.use('/css', express.static(__dirname + '/app/styles/'));
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css/'));
@@ -9,5 +14,13 @@ app.use('/fonts', express.static(__dirname + '/node_modules/bootstrap/fonts/'));
 app.get('/', function (req, res) {
     res.sendFile(path.resolve(__dirname, 'index.html'));
 });
-app.listen(3000);
+//ADMIN >> NEW TEMPLATE
+app.post('/templates', function (req, res) {
+    MongoClient.connect(dbUrl, function (err, db) {
+        assert.equal(null, err);
+        console.log('Connected to mongodb');
+        console.log(req);
+        db.close();
+    });
+});
 //# sourceMappingURL=server.js.map
--- server.ts
+++ server.ts
@@ -1,7 +1,14 @@
 import * as express from 'express';
 import * as path from 'path';
+import * as mongodb from 'mongodb';
+import * as assert from 'assert';
 
-var app = express();
+const MongoClient = mongodb.MongoClient
+const dbUrl = 'mongodb://localhost:27017/qcms' 
+
+
+const app = express();
+app.listen(3000);
 
 app.use(express.static(path.resolve(__dirname)));
 app.use('/css', express.static(__dirname + '/app/styles/'))
@@ -11,4 +18,14 @@ app.use('/fonts', express.static(__dirname + '/node_modules/bootstrap/fonts/'));
 app.get('/', function (req: express.Request, res: express.Response) {
     res.sendFile(path.resolve(__dirname, 'index.html'));
 });
-app.listen(3000);
+
+//ADMIN >> NEW TEMPLATE
+app.post('/templates', function (req: express.Request, res: express.Response) {
+  MongoClient.connect(dbUrl, function(err, db) {
+    assert.equal(null, err);
+    console.log('Connected to mongodb')
+    console.log(req)
+
+    db.close();
+  })
+})
