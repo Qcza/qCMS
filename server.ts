900a203f6a72e24decf88eddeb807a6aff41e7c9
pku <maciejbal90@gmail.com>
2016-08-17 16:24:55 +0200

    Reverse to prevoiusly bootstrap.

--- server.ts
+++ server.ts
@@ -12,8 +12,7 @@ const app = express();
 app.use('/', express.static(__dirname));
 app.use('/css', express.static(__dirname + '/app/styles/'))
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css/'));
-app.use('/css', express.static(__dirname + '/static/font-awesome/css'));
-app.use('/fonts', express.static(__dirname + '/static/font-awesome/fonts'));
+app.use('/fonts', express.static(__dirname + '/node_modules/bootstrap/fonts/'));
 
 // app.get('/', function (req: express.Request, res: express.Response) {
 //     res.sendFile(path.resolve(__dirname, 'index.html'));
