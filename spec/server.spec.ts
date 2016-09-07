4b3733a27900a0fa2700899ff64d13b109d2cca6
pku <pawel.kuczynski@ue.wroc.pl>
2016-09-07 13:14:59 +0200

    Unit Tests Pattern

--- spec/server.spec.ts
+++ spec/server.spec.ts
@@ -5,27 +5,11 @@ import { Template, Element } from '../app/models/template';
 const port = 3000;
 const main_url = `http://localhost:${port}`;
 
-describe('Landing page', function() {
-    describe('GET /', function () {
-        it('return status code 200', function () {
-            request.get(main_url, function (error, response, body) {
-            expect(response.statusCode).toBe(200);
-            })
-        })
-    })
-})
-
-const templates_url = main_url + '/templates';
-
-describe('Templates', function() {
-  describe('POST', function () {
-    it('insert new template do collection', function () {
-      let dummyElement:Element = new Element();
-      let elements:Array<Element> = [ dummyElement ];
-      let dummyTemplate:Template = new Template('dummyTemplate', elements)
-      request.post(templates_url, dummyTemplate, function (error, response, body) {
-        expect(body.response).toBe('test');
-      })
-    })
-  })
-})
+describe("Landing page", function() {
+  it("should return status code 200", function(done) {
+    request(main_url, function (err, res, body) {
+      expect(res.statusCode).toBe(200);
+      done();
+    });
+  });
+});
