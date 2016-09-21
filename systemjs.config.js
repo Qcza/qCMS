5f2a9d988d91c2065450eeaba10278c1730c21ff
Qcza <qcza12@gmail.com>
2016-09-21 17:40:54 +0200

    *update migrate to Angular 2 Final

--- systemjs.config.js
+++ systemjs.config.js
@@ -1,78 +1,58 @@
-
-(function(global) {
+(function (global) {
+System.config({
+  paths: {
+    // paths serve as alias
+    'npm:': 'node_modules/'
+  },
   // map tells the System loader where to look for things
-  var map = {
-    'app':                        'app', // 'dist',
-    '@angular':                   'node_modules/@angular',
-    'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
-    'rxjs':                       'node_modules/rxjs',
-    '@ng-bootstrap':              'node_modules/@ng-bootstrap',
-    '@ng-bootstrap/ng-bootstrap': 'node_modules/@ng-bootstrap/ng-bootstrap',
-    'ng2-cookies':                'node_modules/ng2-cookies',
-    'ng2-uploader':               'node_modules/ng2-uploader',
-  };
+  map: {
+    // our app is within the app folder
+    app: 'app',
+    // angular bundles
+    '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
+    '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
+    '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
+    '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
+    '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+    '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
+    '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
+    '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
+    // other libraries
+    'rxjs':                       'npm:rxjs',
+    'angular2-in-memory-web-api': 'npm:angular2-in-memory-web-api',
+    'ng2-cookies':                'npm:ng2-cookies',
+    'ng2-uploader':               'npm:ng2-uploader',
+    'lodash':                     'npm:lodash',
+    '@ng-bootstrap':              'npm:@ng-bootstrap',
+    '@ng-bootstrap/ng-bootstrap': 'npm:@ng-bootstrap/ng-bootstrap/bundles'
+  },
   // packages tells the System loader how to load when no filename and/or no extension
-  var packages = {
-    'app':                        { main: 'main.js',  defaultExtension: 'js' },
-    'rxjs':                       { defaultExtension: 'js' },
-    'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' },
-    '@ng-bootstrap/ng-bootstrap': { main: 'index.js', defaultExtension: 'js' },
-    'ng2-cookies':                { defaultExtension: 'js' },
-    'ng2-uploader':               { defaultExtension: 'js' },
-  };
-  var ngPackageNames = [
-    'common',
-    'compiler',
-    'core',
-    'forms',
-    'http',
-    'platform-browser',
-    'platform-browser-dynamic',
-    'router',
-    'router-deprecated',
-    'upgrade',
-  ];
-  // Bootstrap packages
-  var ngBootstrapPackageNames = [
-    'accordion',
-    'alert',
-    'bundles',
-    'buttons',
-    'carousel',
-    'collapse',
-    'dropdown',
-    'esm',
-    'modal',
-    'pagination',
-    'popover',
-    'progressbar',
-    'rating',
-    'tabset',
-    'timepicker',
-    'tooltip',
-    'typeahead',
-    'util'
-  ];
-  // Individual files (~300 requests):
-  function packIndex(pkgName) {
-    packages['@angular/'+pkgName] = { main: 'index.js', defaultExtension: 'js' };
+  packages: {
+    app: {
+      main: './main.js',
+      defaultExtension: 'js'
+    },
+    rxjs: {
+      defaultExtension: 'js'
+    },
+    'angular2-in-memory-web-api': {
+      main: './index.js',
+      defaultExtension: 'js'
+    },
+    'ng2-cookies': { 
+      defaultExtension: 'js' 
+    },
+    'ng2-uploader': { 
+      defaultExtension: 'js' 
+    }, 
+    'lodash': { 
+      main: './index.js', 
+      defaultExtension: 'js' 
+    },
+    '@ng-bootstrap/ng-bootstrap': {
+      main: './ng-bootstrap.js',
+      defaultExtension: 'js'
+    }
   }
-  // Bundled (~40 requests):
-  function packUmd(pkgName) {
-    packages['@angular/'+pkgName] = { main: '/bundles/' + pkgName + '.umd.js', defaultExtension: 'js' };
-  }
-  function ngBootstrapPackIndex(pkgName) {
-    packages['@ng-bootstrap/ng-bootstrap/' + pkgName] = {main: 'index.js', defaultExtension: 'js'};
-  }
-  // Most environments should use UMD; some (Karma) need the individual index files
-  var setPackageConfig = System.packageWithIndex ? packIndex : packUmd;
-  // Add package entries for angular packages
-  ngPackageNames.forEach(setPackageConfig);
-  ngBootstrapPackageNames.forEach(ngBootstrapPackIndex);
-
-  var config = {
-    map: map,
-    packages: packages
-  };
-  System.config(config);
+});
 })(this);
